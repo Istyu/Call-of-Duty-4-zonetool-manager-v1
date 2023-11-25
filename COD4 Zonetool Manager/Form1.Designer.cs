a245f24db92fea609e1810a881411785471581ce
Stealth <50705070+Istyu@users.noreply.github.com>
2023-11-25 01:21:30 +0100

    Add files via upload

---
+++ COD4 Zonetool Manager/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace COD4_Zonetool_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dllSelButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dllInjButton = new System.Windows.Forms.Button();
+            this.refreshProcButton = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.instruction2Label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.exit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(92, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COD4 Zonetool Manager";
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.SystemColors.Window;
+            this.exit.Location = new System.Drawing.Point(349, 9);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(26, 29);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "X";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(6, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(273, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Path to zonetoolDLL";
+            // 
+            // dllSelButton
+            // 
+            this.dllSelButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.dllSelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dllSelButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.dllSelButton.Location = new System.Drawing.Point(285, 76);
+            this.dllSelButton.Name = "dllSelButton";
+            this.dllSelButton.Size = new System.Drawing.Size(95, 26);
+            this.dllSelButton.TabIndex = 2;
+            this.dllSelButton.Text = "Choose DLL";
+            this.dllSelButton.UseVisualStyleBackColor = false;
+            this.dllSelButton.Click += new System.EventHandler(this.dllSelButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(374, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // dllInjButton
+            // 
+            this.dllInjButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.dllInjButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dllInjButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.dllInjButton.Location = new System.Drawing.Point(6, 138);
+            this.dllInjButton.Name = "dllInjButton";
+            this.dllInjButton.Size = new System.Drawing.Size(374, 28);
+            this.dllInjButton.TabIndex = 4;
+            this.dllInjButton.Text = "Inject DLL";
+            this.dllInjButton.UseVisualStyleBackColor = false;
+            this.dllInjButton.Click += new System.EventHandler(this.dllInjButton_Click);
+            // 
+            // refreshProcButton
+            // 
+            this.refreshProcButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.refreshProcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshProcButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.refreshProcButton.Location = new System.Drawing.Point(6, 172);
+            this.refreshProcButton.Name = "refreshProcButton";
+            this.refreshProcButton.Size = new System.Drawing.Size(374, 26);
+            this.refreshProcButton.TabIndex = 5;
+            this.refreshProcButton.Text = "Refresh Process List";
+            this.refreshProcButton.UseVisualStyleBackColor = false;
+            this.refreshProcButton.Click += new System.EventHandler(this.refreshProcButton_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.infoLabel.Location = new System.Drawing.Point(12, 425);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(142, 16);
+            this.infoLabel.TabIndex = 6;
+            this.infoLabel.Text = "zonetool manager v1.0";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(12, 368);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(140, 16);
+            this.instructionLabel.TabIndex = 7;
+            this.instructionLabel.Text = "Supported processes:";
+            // 
+            // instruction2Label
+            // 
+            this.instruction2Label.AutoSize = true;
+            this.instruction2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instruction2Label.Location = new System.Drawing.Point(12, 392);
+            this.instruction2Label.Name = "instruction2Label";
+            this.instruction2Label.Size = new System.Drawing.Size(100, 16);
+            this.instruction2Label.TabIndex = 8;
+            this.instruction2Label.Text = "Supported dll\'s:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 450);
+            this.Controls.Add(this.instruction2Label);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.refreshProcButton);
+            this.Controls.Add(this.dllInjButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dllSelButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zonetool Manager";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button dllSelButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button dllInjButton;
+        private System.Windows.Forms.Button refreshProcButton;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label instruction2Label;
+    }
+}
+
---
+++ COD4 Zonetool Manager/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace COD4_Zonetool_Manager
+{
+    public partial class Form1 : Form
+    {
+        private const string ExpectedDllName = "zoneiw3.dll";
+        private const string ExpectedProcessNames = "zonetool,iw3mp,iw3sp";
+        private bool alreadyAdded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            instructionLabel.Text = "Supported processes: " + ExpectedProcessNames;
+            instruction2Label.Text = "Supported dll\'s: " + ExpectedDllName;
+
+            comboBox1.Text = "Choose the cod4 process...";
+            comboBox1.Items.Clear();
+            processBoxElements();
+        }
+
+        private static string DLLP
+        {
+            get;
+            set;
+        }
+
+        private void processBoxElements()
+        {
+            if( alreadyAdded )
+                return;
+            Process[] PC = Process.GetProcesses().Where(p => (long)p.MainWindowHandle != 0).ToArray();
+            foreach( Process p in PC )
+            {
+                if( p.ProcessName == "zonetool" || p.ProcessName == "iw3mp" || p.ProcessName == "iw3sp" )
+                {
+                    alreadyAdded = true;
+                    comboBox1.Items.Add( p.ProcessName );
+                }
+            }
+        }
+
+        // Exit application
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        // Choose DLL
+        private void dllSelButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.InitialDirectory = @"C:\";
+                open.Title = "Locate zoneiw3.dll File For Injection"; // Selection window title
+                open.Filter = "DLL files (*.dll)|*.dll;";
+                open.DefaultExt = "dll";
+                open.CheckFileExists = true;
+                open.CheckPathExists = true;
+                open.Multiselect = false;
+
+                if( open.ShowDialog() == DialogResult.OK )
+                {
+                    if( Path.GetFileName(open.FileName) == ExpectedDllName )
+                    {
+                        textBox1.Text = open.FileName;
+                        DLLP = open.FileName;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Please select the '{ExpectedDllName}' DLL file.", "Invalid DLL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch( Exception ed )
+            {
+                MessageBox.Show( ed.Message );
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DLLP = textBox1.Text;
+        }
+
+        // Refresh Process
+        private void refreshProcButton_Click(object sender, EventArgs e)
+        {
+            processBoxElements();
+        }
+
+        // Inject dll
+        private void dllInjButton_Click(object sender, EventArgs e)
+        {
+            int Result = Inject( comboBox1.Text, DLLP );
+
+            if( Result == 1 )
+                MessageBox.Show( "File Does not Exist" );
+            else if( Result == 2 )
+                MessageBox.Show( "Process not valid" );
+            else if( Result == 3 )
+                MessageBox.Show( "Process Does not Exist" );
+            else if( Result == 4 )
+                MessageBox.Show( "Injection Fails" );
+            else if( Result == 5 )
+                MessageBox.Show( "Injection Succeeded" );
+        }
+
+        static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern IntPtr OpenProcess( uint dwDesiredAccess, int bInheritHandle, uint dwProcessId );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern int CloseHandle( IntPtr hObject );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern IntPtr GetProcAddress( IntPtr hModule, string lpProcName );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern IntPtr GetModuleHandle( string lpModuleName );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern IntPtr VirtualAllocEx( IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern int WriteProcessMemory( IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten );
+
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        static extern IntPtr CreateRemoteThread( IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId );
+
+        public static int Inject( string PN, string DLLP )
+        {
+            // 1 = file does not exist
+            // 2 = The selected process is not the call of duty 4
+            // 3 = process not active
+            // 4 = injection failed
+            // 5 = injection succeeded
+
+            if( !File.Exists( DLLP ) )
+                return 1;
+
+            if( !ExpectedProcessesRunning() )
+                return 2;
+
+            uint _procId = 0;
+            Process[] _procs = Process.GetProcesses();
+            for( int i = 0; i < _procs.Length; i++ )
+            {
+                if( _procs[i].ProcessName == PN )
+                {
+                    _procId = (uint)_procs[i].Id;
+                    break;
+                }
+            }
+
+            if( _procId == 0 )
+                return 3;
+
+            if( !SI( _procId, DLLP ) )
+                return 4;
+
+            return 5;
+        }
+
+        public static bool SI( uint P, string DLLP )
+        {
+            IntPtr hndProc = OpenProcess( ( 0x2 | 0x8 | 0x10 | 0x20 | 0x400 ), 1, P );
+
+            if( hndProc == INTPTR_ZERO )
+                return false;
+
+            IntPtr lpLLAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+ 
+            if( lpLLAddress == INTPTR_ZERO )
+                return false;
+
+            IntPtr lpAddress = VirtualAllocEx( hndProc, (IntPtr)null, (IntPtr)DLLP.Length, ( 0x1000 | 0x2000 ), 0x40 );
+
+            if( lpAddress == INTPTR_ZERO )
+                return false;
+
+            byte[] bytes = Encoding.ASCII.GetBytes( DLLP );
+
+            if( WriteProcessMemory( hndProc, lpAddress, bytes, (uint)bytes.Length, 0 ) == 0 )
+                return false;
+
+            if (CreateRemoteThread(hndProc, (IntPtr)null, INTPTR_ZERO, lpLLAddress, lpAddress, 0, (IntPtr)null) == INTPTR_ZERO)
+                return false;
+
+            CloseHandle( hndProc );
+
+            return true;
+        }
+
+        private static bool ExpectedProcessesRunning()
+        {
+            string[] processNames = ExpectedProcessNames.Split(',');
+        
+            foreach( string processName in processNames )
+            {
+                // Verify that the process is running
+                Process[] processes = Process.GetProcessesByName(processName);
+                if( processes.Length > 0 )
+                    return true; // At least one expected process is running
+            }
+        
+            return false; // None of the expected processes are running
+        }
+    }
+}
